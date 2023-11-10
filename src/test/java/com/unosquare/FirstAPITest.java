596b72d50f5f231aba6e1f06063a2bb20616c1c8
Kevin Herrera <kevin.herrera@unosquare.com>
2023-11-10 19:16:27 -0400

    project basic test file setup

---
+++ src/test/java/com/unosquare/FirstAPITest.java
@@ -0,0 +1,9 @@
+package com.unosquare;
+
+import org.testng.annotations.BeforeMethod;
+
+public class FirstAPITest {
+	@BeforeMethod
+	public void beforeMethod() {
+	}
+}
---
+++ src/test/java/com/unosquare/SecondAPITest.java
@@ -0,0 +1,9 @@
+package com.unosquare;
+
+import org.testng.annotations.BeforeMethod;
+
+public class SecondAPITest {
+	@BeforeMethod
+	public void beforeMethod() {
+	}
+}
